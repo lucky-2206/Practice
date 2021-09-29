e3684a8fb831b036156b90e33430e94b44483bc7
Vaibhav Jain <88602975+lucky-2206@users.noreply.github.com>
2021-09-30 00:53:26 +0530

    Update outer.java

--- src/com/company/outer.java
+++ src/com/company/outer.java
@@ -1,5 +1,6 @@
 package com.company;
 
+//Way 1:
 public class outer {
     static int n2=20;
     public static class inner
@@ -16,5 +17,30 @@ public class outer {
     public static void main(String[] args) {
         outer.inner obj = new outer.inner();
         obj.display();
+    }
+}
+
+//way 2
+package com.company;
+
+public class outer {
+    int n2=20;
+     class inner
+    {
+        int n2=30;
+        public void display() {
+            int n2=40;
+            System.out.println(n2);
+            System.out.println(this.n2);
+            System.out.println(outer.this.n2);
+        }
+    }
+
+    public static void main(String[] args) {
+         outer ob =new outer();
+         outer.inner ob1 = ob.new inner();
+        //we can write these statement in one line as:
+        // outer.inner obj = new outer().new inner();
+        ob1.display();
     }
 }
