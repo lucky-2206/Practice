555fdef9fff126c8cee94cefc693ba8059d27019
lucky_2206 <vaibhavjainlucky98@gmail.com>
2021-11-09 16:46:07 +0530

    Initial Commit

---
+++ src/WrapperClass/MaxMin.java
@@ -0,0 +1,26 @@
+package WrapperClass;
+
+public class MaxMin {
+    public static void main(String[] args) {
+
+    System.out.println("Max and min values of different data types are:");
+        System.out.println("Byte :");
+        System.out.println("max:"+Byte.MAX_VALUE);
+        System.out.println("min:"+Byte.MIN_VALUE);
+        System.out.println("Short :");
+        System.out.println("max:"+Short.MAX_VALUE);
+        System.out.println("min:"+Short.MIN_VALUE);
+        System.out.println("Integer :");
+        System.out.println("max:"+Integer.MAX_VALUE);
+        System.out.println("min:"+Integer.MIN_VALUE);
+        System.out.println("Long :");
+        System.out.println("max:"+Long.MAX_VALUE);
+        System.out.println("min:"+Long.MIN_VALUE);
+        System.out.println("Float :");
+        System.out.println("max:"+Float.MAX_VALUE);
+        System.out.println("min:"+Float.MIN_VALUE);
+        System.out.println("Double :");
+        System.out.println("max:"+Double.MAX_VALUE);
+        System.out.println("min:"+Double.MIN_VALUE);
+}
+}
