9140fe326deedb83765bbe03a114a815e636f955
lucky_2206 <vaibhavjainlucky98@gmail.com>
2021-11-17 23:31:46 +0530

    Initial Commit

---
+++ src/strings/program3.java
@@ -0,0 +1,23 @@
+package strings;
+
+import java.util.*;
+public class program3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String s= sc.nextLine();
+        System.out.println(new_string(s));
+    }
+    static String new_string(String s)
+    {
+        String s2="";
+        if(s.length()>1) {
+            for (int i = 0; i < s.length(); i++) {
+                s2 = s2 + s.charAt(0) + s.charAt(1);
+            }
+            return s2;
+        }
+        else
+            return s;
+    }
+
+}
