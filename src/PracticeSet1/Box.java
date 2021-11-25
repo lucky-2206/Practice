f606df87951cfadfb3697df4e01d5c1bee0ff287
lucky_2206 <vaibhavjainlucky98@gmail.com>
2021-11-25 22:29:52 +0530

    Initial Commit

---
+++ src/PracticeSet1/Box.java
@@ -0,0 +1,21 @@
+package PracticeSet1;
+
+public class Box {
+    int width, height, depth;
+
+    public Box(int width, int height, int depth) {
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+    }
+
+    public int volume() {
+        return width * height * depth;
+    }
+
+
+    public static void main(String[] args) {
+        Box ob = new Box(7, 15, 12);
+        System.out.println("volume of the box is equal to = "+ob.volume()+" unit cube.");
+    }
+}
