a3f9f1f3dbf25a2ce9693784d3466281995ca09c
lucky_2206 <vaibhavjainlucky98@gmail.com>
2021-12-08 23:49:07 +0530

    Initial Commit

---
+++ src/PracticeSet3/Calculator.java
@@ -0,0 +1,17 @@
+package PracticeSet3;
+
+public class Calculator {
+    public static int powerInt(int num1,int num2)
+    {
+        return (int)Math.pow(num1,num2);
+    }
+    public static Double powerDouble(Double num1,int num2)
+    {
+        return Math.pow(num1,num2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Calculator.powerInt(3,4));
+        System.out.println(Calculator.powerDouble(2.4,3));
+    }
+}
